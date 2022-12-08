45c6300cdb7582e03dd92d93e3cf523a6fa1a42e
fernando britto <fernandobritto925@gmail.com>
2022-12-08 20:10:18 -0300

    fixed mongose url

--- api.js
+++ api.js
@@ -5,9 +5,7 @@ const app = express()
 const { Auth, isAuthenticated } = require('./auth.controller')
 const port = process.env.PORT || 3000
 
-console.log(process.env.PORT)
-
-mongoose.connect(process.env.SECRET_LINK)
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kqg0zsw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
 
 app.use(express.json())
 
